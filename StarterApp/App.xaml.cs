bc813df49774a2581dfd179b1261e9511e0889fc
mmoha-199 <mmyacont20@hotmail.com>
2026-05-01 21:20:40 +0000

    fixing connection strings

--- StarterApp/App.xaml.cs
+++ StarterApp/App.xaml.cs
@@ -11,6 +11,7 @@ public partial class App : Application
 		InitializeComponent();
 
 		Routing.RegisterRoute(nameof(Views.MainPage), typeof(Views.MainPage));
+		Routing.RegisterRoute(nameof(Views.ItemDetailPage), typeof(Views.ItemDetailPage));
 		Routing.RegisterRoute(nameof(Views.LoginPage), typeof(Views.LoginPage));
 		Routing.RegisterRoute(nameof(Views.RegisterPage), typeof(Views.RegisterPage));
 		Routing.RegisterRoute(nameof(Views.UserListPage), typeof(Views.UserListPage));
